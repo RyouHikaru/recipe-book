2ad9fc84870b8d5dc547d2895ea9078d7b5b5324
RyouHikaru <carljamesreblora13@gmail.com>
2023-03-20 10:04:20 +0800

    Updated recipe-related components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'Cookie',
+      'A test recipe for cookie',
+      'https://i.stack.imgur.com/97Y6o.jpg'
+    ),
+  ];
 }
