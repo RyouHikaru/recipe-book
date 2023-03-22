2895b72bf7fb392c1f5113509b1f7901e5a5a909
RyouHikaru <carljamesreblora13@gmail.com>
2023-03-22 13:41:36 +0800

    Added custom Dropdown directive

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,16 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  textContent = '☰';
   collapsed = true;
   @Output() featureSelected = new EventEmitter<string>();
   
   onSelect(feature: string) {
     this.featureSelected.emit(feature);
+  }
+
+  toggleCollapse() {
+    this.collapsed = !this.collapsed;
+    this.textContent = this.collapsed ? '☰' : '✖';
   }
 }
